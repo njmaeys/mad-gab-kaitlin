7142607b5bbd3e7a9d0f424776c734be4edb6847
njmaeys <nmaeys@hotmail.com>
2021-01-24 13:47:48 -0600

    host

--- madgab/madgab/settings.py
+++ madgab/madgab/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['172.17.61.122', '192.168.86.51']
 
 
 # Application definition
